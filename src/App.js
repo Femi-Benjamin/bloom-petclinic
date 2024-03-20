2ea59c74722379033a6c5f3aed54521201ddcbb8
Olufemi Benjamin <benjaminolufemi16@gmail.com>
2024-03-20 16:42:40 -0500

    V4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeNav from "./NavBar/HomeNav";
 import Home from "./Pages/Home";
 import Owners from "./Pages/Owners";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Veterniarians from "./Pages/Veterniarians";
 
 const App = () => {
